5502dd75ee672fc2edec60f6d9e07515ac564617
nikkonrom <nikkonrom@gmail.com>
2017-10-26 14:08:03 +0300

    Image scaling working!

--- python/imager.py
+++ python/imager.py
@@ -15,26 +15,26 @@ class MyWin(QtWidgets.QMainWindow):
         # Здесь прописываем событие нажатия на кнопку
         self.ui.loadButton.clicked.connect(self.loadImage)
         self.ui.boundariesButton.clicked.connect(self.getBoundaries)
-        self.ui.keepRatio.stateChanged.connect(self.ui.label.setKeepAspectRatioEnabled)
-        self.ui.keepRatio.stateChanged.connect(self.ui.label_2.setKeepAspectRatioEnabled)
-        self.ui.originalSize.stateChanged.connect(self.ui.label.setOverscaleEnabled)
-        self.ui.originalSize.stateChanged.connect(self.ui.label_2.setOverscaleEnabled)
+        self.ui.keepAspectRatioCheckBox.stateChanged.connect(self.ui.label.setKeepAspectRatioEnabled)
+        self.ui.keepAspectRatioCheckBox.stateChanged.connect(self.ui.label_2.setKeepAspectRatioEnabled)
+        self.ui.ignoreOverscaleCheckBox.stateChanged.connect(self.ui.label.setOverscaleEnabled)
+        self.ui.ignoreOverscaleCheckBox.stateChanged.connect(self.ui.label_2.setOverscaleEnabled)
 
     # Пока пустая функция которая выполняется
     # при нажатии на кнопку
     def loadImage(self):
         inputImagePath = QFileDialog.getOpenFileName(self, 'Open image...', '/home')[0]
-        inputPixmap = Qt.QPixmap(inputImagePath)
-        self.ui.label.setPixmap(inputPixmap)
-    
+        if inputImagePath:
+            inputPixmap = Qt.QPixmap(inputImagePath)
+            self.ui.label.loadPixmapData(inputPixmap)
+
     def getBoundaries(self):
-        if self.ui.label.pixmap():
-            self.ui.label_2.setPixmap(BoundariesOperation.Execute(self.ui.label.pixmap()))
-        
+        if self.ui.label.getPixmap() is not None:
+            self.ui.label_2.loadPixmapData(BoundariesOperation.Execute(self.ui.label.getPixmap()))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     myapp = MyWin()
     myapp.show()
     sys.exit(app.exec_())
-
